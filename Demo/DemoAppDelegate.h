af511f6730b7ae078507415757138399e152e8e2
Matthijs Hollemans <mail@hollance.com>
2011-03-19 17:25:44 +0100

    Initial Commit

---
+++ Demo/DemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DemoAppDelegate.h
+//  Demo
+//
+//  Created by Matthijs Hollemans on 19-03-11.
+//  Copyright 2011 Hollance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Matthijs Hollemans on 19-03-11.
+//  Copyright 2011 Hollance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
